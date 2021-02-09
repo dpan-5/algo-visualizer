27ffc8e59e5982d097dfed2c0ea65df0fb705daa
David Pan <dpan5425@gmail.com>
2021-02-09 14:50:29 -0500

    Fix swap function for selection sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -6,8 +6,8 @@ import './SortingVisualizer.css';
 export default function SortingVisualizer() {
     
     const [array, setArray] = useState([]);
-    const [resetClicked, setResetClicked] = useState(false);
     const [algoIsRunning, setAlgoIsRunning] = useState(false);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         resetArray();
@@ -20,7 +20,8 @@ export default function SortingVisualizer() {
 
         const array = [];
 
-        for (let i = 0; i < 23; i ++) {
+        //23
+        for (let i = 0; i < 5; i ++) {
             array.push(Math.floor(Math.random() * (100 - 5 + 1) + 5));
         }
 
@@ -30,8 +31,8 @@ export default function SortingVisualizer() {
 
     const handleSortClick = (e) => {
         switch (e.target.id) {
-            case 'merge-sort':
-                console.log('1');
+            case 'selection-sort':
+                selectionSort();
                 break;
             case 'quick-sort':
                 console.log('2');
@@ -67,13 +68,55 @@ export default function SortingVisualizer() {
           });
     }
 
+    const swap2 = async (bar1, bar2) => {
+        return new Promise(resolve => {
+            const style1 = window.getComputedStyle(bar1);
+            const style2 = window.getComputedStyle(bar2);
+        
+            const transform1 = style1.getPropertyValue("transform");
+            const transform2 = style2.getPropertyValue("transform");
+        
+            bar1.style.transform = transform2;
+            bar2.style.transform = transform1;
+        
+
+            window.requestAnimationFrame(function() {
+              setTimeout(() => {
+                // create temp element and insert it before bar1
+                const temp = document.createElement('div');
+                container.insertBefore(temp, bar1);
+                
+                // move bar1 to right before bar2
+                container.insertBefore(bar1, bar2);
+
+                // move bar2 to right before bar1 used to be (now temp)
+                container.insertBefore(bar2, temp);
+
+                // remove temp element node
+                container.removeChild(temp);
+
+                resolve();
+              }, 250);
+            });
+          });
+    }
+
 
     const bubbleSort = async (delay = 100) => {
         setAlgoIsRunning(true);
         let bars = document.querySelectorAll('.array-bar');
 
         for (let i = 0; i < bars.length; i++) {
+            // document.querySelector('.iLoop').style.color = 'red';
+            // document.querySelector('.jLoop').style.color = 'black';
+
+            await new Promise(resolve => setTimeout(() => {
+                resolve();
+            }, delay));
+
             for (let j = 0; j < bars.length - 1 - i; j++) {
+                // document.querySelector('.iLoop').style.color = 'black';
+                // document.querySelector('.jLoop').style.color = 'red';
                 bars[j].style.backgroundColor = "#FF4949";
                 bars[j + 1].style.backgroundColor = "#FF4949";
 
@@ -85,6 +128,9 @@ export default function SortingVisualizer() {
                 const value2 = Number(bars[j + 1].childNodes[0].innerHTML);
 
                 if (value1 > value2) {
+                    // document.querySelector('.iLoop').style.color = 'black';
+                    // document.querySelector('.jLoop').style.color = 'black';
+                    
                     await swap(bars[j], bars[j + 1]);
                     bars = document.querySelectorAll(".array-bar");
                 }
@@ -96,6 +142,70 @@ export default function SortingVisualizer() {
             bars[bars.length - i - 1].style.backgroundColor = "#13CE66";
         }
     }
+
+    //===================================================================================
+    // SELECTION SORT
+    //===================================================================================
+
+    const selectionSort = async (delay = 1000) => {
+        setAlgoIsRunning(true);
+        let bars = document.querySelectorAll('.array-bar');
+
+
+        for (let i = 0; i < bars.length; i++) {
+            console.log(bars);
+            let min = i;
+
+            bars[i].style.backgroundColor = "#FF4949";
+
+            await new Promise(resolve => setTimeout(() => {
+                resolve();
+            }, delay));
+
+            for (let j = i + 1; j < bars.length; j++) {
+                bars[j].style.backgroundColor = "yellow";
+
+                await new Promise(resolve => setTimeout(() => {
+                    resolve();
+                }, delay));
+
+
+                if (Number(bars[j].childNodes[0].innerHTML) < Number(bars[min].childNodes[0].innerHTML)) {
+                    if (min !== i) {
+                        // Turn old index of min back to blue
+                        bars[min].style.backgroundColor= "#58B7FF";
+                    }
+                    // Set new min object to current min of index j
+                    min = j;
+                    // Turn current index (new min) to purple
+                    bars[j].style.backgroundColor = '#AE58FF';
+                } else {
+                    bars[j].style.backgroundColor = "#58B7FF";
+                }
+            }
+
+            if (min !== i) {
+                await swap2(bars[i], bars[min]);
+                bars = document.querySelectorAll(".array-bar");
+                bars[i].style.backgroundColor = "#58B7FF";
+            }
+
+
+
+            bars[i].style.backgroundColor = "#13CE66";
+
+
+        }
+
+    }
+
+
+
+
+    //===================================================================================
+    // RENDER
+    //===================================================================================
+
 
     
     return (
@@ -111,11 +221,29 @@ export default function SortingVisualizer() {
             </div>
             <div className='button-container'>
                 <button onClick={() => resetArray()} className='reset-button'>Reset</button>
-                <button onClick={handleSortClick} id='merge-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Merge Sort</button>
+                <button onClick={handleSortClick} id='selection-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Selection Sort</button>
+                <button onClick={handleSortClick} id='bubble-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Bubble Sort</button>
                 <button onClick={handleSortClick} id='quick-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Quick Sort</button>
                 <button onClick={handleSortClick} id='heap-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Heap Sort</button>
-                <button onClick={handleSortClick} id='bubble-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Bubble Sort</button>
             </div>
+            {/* <code className='iLoop'>
+                {"for (let i = 0; i < array.length; i++) {"}
+            </code>
+            <code className='jLoop' style={{textIndent: 30}}>
+                {"for (let j = 0; j < array.length - 1 - j; j++) {"}
+            </code>
+            <code className='ifCheck' style={{textIndent: 60}}>
+                {"if (array[j] > array[j + 1]) {"}
+            </code>
+            <code className='ifCheckTrue' style={{textIndent: 90}}>
+                {"let tmp = array[j]"}
+            </code>
+            <code className='ifCheckTrue' style={{textIndent: 90}}>
+                {"array[j] = array[j + 1]"}
+            </code>
+            <code className='ifCheckTrue' style={{textIndent: 90}}>
+                {"array[j + 1] = tmp"}
+            </code> */}
         </div>
     );
 }
