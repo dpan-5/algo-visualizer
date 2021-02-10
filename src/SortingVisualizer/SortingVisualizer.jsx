4845e04e2554b63d063f11db78213bb18fb7f7c4
David Pan <dpan5425@gmail.com>
2021-02-09 23:17:03 -0500

    somehow got quick sort working

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,12 +2,17 @@ import { useState, useEffect } from 'react';
 import './SortingVisualizer.css';
 // import { bubbleSort } from './../SortingAlgos/SortingAlgos';
 
+const ARRAY_SIZE = 9;
+
+const DELAY_MS = 100;
 
 export default function SortingVisualizer() {
     
     const [array, setArray] = useState([]);
     const [algoIsRunning, setAlgoIsRunning] = useState(false);
     const [algoHasRan, setAlgoHasRan] = useState(false);
+
+
 
     useEffect(() => {
         resetArray();
@@ -23,30 +28,30 @@ export default function SortingVisualizer() {
         if (algoIsRunning) {
             window.location.href='/';
         }
-
         if (algoHasRan) {
             let bars = document.querySelectorAll('.array-bar');
             bars.forEach(bar => bar.style.backgroundColor = '#58B7FF');
         }
-
         const array = [];
-
-        //23
-        for (let i = 0; i < 5; i ++) {
+        for (let i = 0; i < ARRAY_SIZE; i ++) {
             array.push(Math.floor(Math.random() * (100 - 5 + 1) + 5));
         }
-
-        setArray(array);
-        
+        setArray(array); 
     }
 
     const handleSortClick = (e) => {
+        setAlgoIsRunning(true);
+        if (algoHasRan) {
+            let bars = document.querySelectorAll('.array-bar');
+            bars.forEach(bar => bar.style.backgroundColor = '#58B7FF')
+        }
+
         switch (e.target.id) {
             case 'selection-sort':
                 selectionSort();
                 break;
             case 'quick-sort':
-                console.log('2');
+                quickSortIterative(array);
                 break;
             case 'heap-sort':
                 console.log('3');
@@ -112,14 +117,12 @@ export default function SortingVisualizer() {
           });
     }
 
+    //===================================================================================
+    // BUBBLE SORT
+    //===================================================================================
 
     const bubbleSort = async (delay = 100) => {
-        setAlgoIsRunning(true);
         let bars = document.querySelectorAll('.array-bar');
-
-        if (algoHasRan) {
-            bars.forEach(bar => bar.style.backgroundColor = '#58B7FF')
-        }
 
         for (let i = 0; i < bars.length; i++) {
             // document.querySelector('.iLoop').style.color = 'red';
@@ -165,15 +168,10 @@ export default function SortingVisualizer() {
     //===================================================================================
 
     const selectionSort = async (delay = 100) => {
-        setAlgoIsRunning(true);
         let bars = document.querySelectorAll('.array-bar');
 
-        if (algoHasRan) {
-            bars.forEach(bar => bar.style.backgroundColor = '#58B7FF')
-        }
 
         for (let i = 0; i < bars.length; i++) {
-            console.log(bars);
             let min = i;
 
             bars[i].style.backgroundColor = "#FF4949";
@@ -224,6 +222,135 @@ export default function SortingVisualizer() {
         setAlgoIsRunning(false);
     }
 
+    //===================================================================================
+    // QUICK SORT
+    //===================================================================================
+
+    const partition = async (arr, start, end) => {
+        
+        const pivotValue = arr[end];
+        let pivotIndex = start;
+
+        for (let i = start; i < end; i++) {
+            if (arr[i] < pivotValue) {
+                // Swap elements
+
+                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+                // Move pivot index to next element
+                pivotIndex++;
+            }
+        }
+
+        [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+        return pivotIndex;
+    }
+
+    const quickSortRecursive = async (arr, start, end) => {
+        // let bars = document.querySelectorAll('.array-bar');
+
+        if (start >= end) {
+            return;
+        }
+
+        let index = await partition(arr, start, end);
+        
+        await Promise.all([quickSortRecursive(arr, start, index - 1), quickSortRecursive(arr, index + 1, end)])
+    }
+
+    const partition2 = async (bars, start, end) => {
+
+        const pivotValue = Number(bars[end].childNodes[0].innerHTML);
+        let pivotIndex = start;
+
+        // await new Promise(resolve => {
+        //     setTimeout(resolve, 1000);
+        // });
+
+        bars[pivotIndex].style.backgroundColor = 'yellow';
+
+        for (let i = start; i < end; i++) {
+            if (bars[i].childNodes[0].innerHTML < pivotValue) {
+                // Swap elements
+
+                await new Promise(resolve => setTimeout(() => {
+                    resolve();
+                }, 1000));
+
+                await swap2(bars[i], bars[pivotIndex]);
+
+                bars = document.querySelectorAll('.array-bar');
+
+                // [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+                // Move pivot index to next element
+                pivotIndex++;
+                bars[pivotIndex - 1].style.backgroundColor = '#58B7FF';
+                bars[pivotIndex].style.backgroundColor = 'yellow';
+            }
+        }
+
+        await new Promise(resolve => setTimeout(() => {
+            resolve();
+        }, 1000));
+
+        await swap2(bars[pivotIndex], bars[end]);
+        bars = document.querySelectorAll('.array-bar');
+        // [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+        return pivotIndex;
+    }
+
+    const quickSortIterative = async (arr) => {
+        let bars = document.querySelectorAll('.array-bar');
+
+        // Creating an array that we'll use as a stack, using the push() and pop() functions
+        let stack = [];
+        
+        // Adding the entire initial array as an "unsorted subarray"
+        stack.push(0);
+        stack.push(bars.length - 1);
+        
+        // There isn't an explicit peek() function
+        // The loop repeats as long as we have unsorted subarrays
+        while(stack[stack.length - 1] >= 0){
+            await new Promise(resolve => {
+                setTimeout(resolve, 100);
+            });
+
+            console.log(stack);
+            // Extracting the top unsorted subarray
+            let end = stack.pop();
+            let start = stack.pop();
+
+            bars[end].style.backgroundColor = 'black';
+            bars[start].style.backgroundColor = 'black';
+            
+            let pivotIndex = await partition2(bars, start, end);
+            console.log(pivotIndex);
+
+            bars = document.querySelectorAll('.array-bar');
+
+            // If there are unsorted elements to the "left" of the pivot,
+            // we add that subarray to the stack so we can sort it later
+            if (pivotIndex - 1 > start){
+                stack.push(start);
+                stack.push(pivotIndex - 1);
+            }
+            
+            // If there are unsorted elements to the "right" of the pivot,
+            // we add that subarray to the stack so we can sort it later
+            if (pivotIndex + 1 < end){
+                stack.push(pivotIndex + 1);
+                stack.push(end);
+            }
+
+            
+
+
+        }
+        console.log(stack);
+        setAlgoIsRunning(false);
+    }
+
+    
 
 
 
@@ -251,6 +378,14 @@ export default function SortingVisualizer() {
                 <button onClick={handleSortClick} id='quick-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Quick Sort</button>
                 <button onClick={handleSortClick} id='heap-sort' disabled={algoIsRunning} style={algoIsRunning ? {cursor: 'not-allowed'} : {}}>Heap Sort</button>
             </div>
+            {/* <input id='slider' type='range' min='0' max='5' /> */}
+
+        </div>
+    );
+}
+
+
+
             {/* <code className='iLoop'>
                 {"for (let i = 0; i < array.length; i++) {"}
             </code>
@@ -269,6 +404,3 @@ export default function SortingVisualizer() {
             <code className='ifCheckTrue' style={{textIndent: 90}}>
                 {"array[j + 1] = tmp"}
             </code> */}
-        </div>
-    );
-}
